47168497406b173c7f6afeba16fa128690cae97d
Waj Pontes <wajpontes@gmail.com>
2026-03-28 23:08:09 -0300

    add telegram

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ BANKROLL = 1000  # valor da sua banca (em reais)
 VALUE_THRESHOLD = 0.05  # mínimo de valor pra apostar
 
 # Telegram (opcional - pode deixar assim por enquanto)
-TELEGRAM_TOKEN = ""
-TELEGRAM_CHAT_ID = ""
+TELEGRAM_TOKEN = "8705713589:AAFdMxxFRHFrAB-3c_JQgnR96shxrnN3CqM"
+TELEGRAM_CHAT_ID = "1209904852"
 API_KEY = "ea6cfd6f29063416f8b3c2723816346a"
